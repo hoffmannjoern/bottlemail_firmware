1862707cfcab67d776c7e4f4f03389175b296dc5
Jörn Hoffmann <j.hoffmann@xceeth.com>
2014-09-14 16:08:52 +0200

    - Add more test cases for CobsFrameTest

--- v2/Bottlemail/bottlemail/unittest/CobsFrameTest.h
+++ v2/Bottlemail/bottlemail/unittest/CobsFrameTest.h
@@ -24,30 +24,31 @@ public:
   
   void testEncodeDecode(void)
   {
-    const int cBufferSize    = 10;
-    uint8_t cBuffer[10]      = {0x65, 0x66, 0x00, 0x68};
-    uint8_t cBufferEncoded[] = {0x03, 0x65, 0x66, 0x02, 0x68};
+    const int payloadLength = 6;
+    uint8_t cBuffer[payloadLength+1] = {0x61, 0x62, 0x63, 0x0, 0x64, 0x65};
+    const uint8_t cBufferDecoded[]   = {0x61, 0x62, 0x63, 0x0, 0x64, 0x65};
+    const uint8_t cBufferEncoded[]   = {0x04, 0x61, 0x62, 0x63, 0x03, 0x64, 0x65};
     
-    Buffer buffer(cBuffer, cBufferSize);
-    CobsFrame frame(buffer, 4, false);
+    Buffer buffer(cBuffer, sizeof(cBuffer));
+    CobsFrame frame(buffer, payloadLength, false);
     
     // Check creation
-    TS_ASSERT_EQUALS(frame.getLength(), 4);
+    TS_ASSERT_EQUALS(frame.getLength(), payloadLength);
     TS_ASSERT_EQUALS(frame.isEncoded(), false);
-    
-    printf("frame: %s\n", cBuffer);
     
     // Check encode
     bool ret = frame.encode();
     TS_ASSERT_EQUALS(ret, true);
-    TS_ASSERT_EQUALS(frame.getLength(), 5);
+    TS_ASSERT_EQUALS(frame.getLength(), payloadLength+1);
     TS_ASSERT_EQUALS(frame.isEncoded(), true);
+    TS_ASSERT_EQUALS(memcmp(cBuffer, cBufferEncoded, sizeof(cBufferEncoded)), 0);
     
-    
+    // Check decode
     ret = frame.decode();
     TS_ASSERT_EQUALS(ret, true);
-    
-    printf("frame %s\n", cBuffer);
+    TS_ASSERT_EQUALS(frame.getLength(), payloadLength);
+    TS_ASSERT_EQUALS(frame.isEncoded(), false);
+    TS_ASSERT_EQUALS(memcmp(cBuffer, cBufferDecoded, sizeof(cBufferDecoded)), 0);
   }
 
 };
